0ffe0b12b26b7a48456f0dd9b4c93234fe37486e
str057 <error.blin@gmail.com>
2025-10-04 16:50:09 +0300

    Delete djenerics directory

--- djenerics/brods/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BrodsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "brods"
--- djenerics/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from users.models import User
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("email", "phone", "city")
