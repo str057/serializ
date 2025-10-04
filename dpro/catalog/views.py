d938380837b39b37165ce1f456855971c5968ede
str057 <error.blin@gmail.com>
2025-10-04 16:46:19 +0300

    Delete dpro directory

--- dpro/catalog/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'catalog/home.html')
-
-def contacts(request):
-    return render(request, 'catalog/contacts.html')
