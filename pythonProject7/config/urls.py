5434216ca4963d05d3aeca1466cb6688b52d02de
str057 <error.blin@gmail.com>
2025-10-04 16:47:20 +0300

    Delete pythonProject7 directory

--- pythonProject7/config/urls.py
+++
@@ -1,9 +0,0 @@
-from xml.etree.ElementInclude import include
-
-from django.contrib import admin
-from dogs.urls import path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include(('dogs.urls', 'dogs')))
-]
--- pythonProject7/dogs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from dogs.apps import DogsConfig
-
-app_name = DogsConfig.name
-
-urlpatterns = [
-
-
-]
